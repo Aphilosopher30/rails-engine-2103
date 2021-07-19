4912233b4fbb92afeb798e1835cd7a5d4a16748a
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-07-19 16:16:33 -0600

    set up tests for merchants

--- Gemfile
+++ Gemfile
@@ -31,6 +31,15 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  gem 'pry'
+  gem 'rspec-rails'
+  # gem 'capybara'
+  gem 'simplecov'
+  gem 'factory_bot_rails'
+  # gem 'faker'
+  gem 'shoulda-matchers', '~> 3.1'
+
 end
 
 group :development do
---
+++ spec/requests/api/v1/merchants/merchants_request_show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant show' do
+
+  describe "merchant show" do
+
+    it 'happy path' do
+
+      merchant_1 = Merchant.create(name: "random_marchant_1")
+      merchant_2 = Merchant.create(name: "random_marchant_2")
+      merchant_3 = Merchant.create(name: "random_marchant_3")
+      merchant_4 = Merchant.create(name: "random_marchant_4")
+      merchant_5 = Merchant.create(name: "random_marchant_5")
+
+      get "/api/v1/merchants/#{merchant_3.id}"
+
+      merchant_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(merchant_response[:data]).to have_key(:id)
+      expect(merchant_response[:data][:id]).to eq(merchant_id)
+      expect(merchant_response[:data][:attributes][:name]).to be_a(String)
+      expect(merchant_response[:data][:attributes]).to have_key(:name)
+    end
+  end
+
+  describe 'sad paths' do
+    it 'if we pass in an id that doesnt exit' do
+      merchant_1 = Merchant.create(name: "random_marchant_1")
+      merchant_2 = Merchant.create(name: "random_marchant_2")
+      merchant_3 = Merchant.create(name: "random_marchant_3")
+
+      not_an_id = merchant_1.id.to_i + merchant_2.id.to_i + merchant_3.id.to_i
+
+      get "/api/v1/merchants/#{not_an_id}"
+
+      response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_a(Hash)
+    end
+  end
+end
